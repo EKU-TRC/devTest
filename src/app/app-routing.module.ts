12fa066bf0da3f737686e1fb9030f3a70b5ed3f1
Kenneth <kenneth.james.carroll@gmail.com>
2020-12-08 14:55:04 -0500

    components set through routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+/**
+ *  App Routing Module
+ * 
+ *  Author: Kenneth Carroll
+ *  Date: 12/8/2020
+ *  revision: 1
+ */
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BudgetCodeFormComponent } from './components/budget-code-form/budget-code-form.component';
+import { BudgetCodesListComponent } from './components/budget-codes-list/budget-codes-list.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: "list", pathMatch: 'full'},
+  {path: 'form', component: BudgetCodeFormComponent},
+  {path: 'list', component: BudgetCodesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/budget-code-form/budget-code-form.component.ts
+++ src/app/components/budget-code-form/budget-code-form.component.ts
@@ -3,7 +3,7 @@
  * 
  * author: Kenneth Carroll
  * date: 12/8/2020
- * revision: 1
+ * revision: 2
  */
 
 // angular imports
@@ -13,15 +13,12 @@ import { Component, OnInit } from '@angular/core';
 import { BudgetCode } from '../../shared/models/budget-code.model';
 import { BudgetCodesService } from '../../shared/services/budget-codes.service';
 
-
-
 @Component({
   selector: 'app-budget-code-form',
   templateUrl: './budget-code-form.component.html',
   styleUrls: ['./budget-code-form.component.css']
 })
 export class BudgetCodeFormComponent implements OnInit {
-
 
   // public budget code for handling the input from the form
   budgetCode: BudgetCode = new BudgetCode(null, null, null, null);
