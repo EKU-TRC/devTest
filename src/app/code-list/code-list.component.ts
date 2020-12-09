e960ed691bd5d1fd17d948a7c12c43f7e3f4698e
Wei Wen <wei.wen0@gmail.com>
2020-12-09 15:07:36 -0500

    added budget codes sort functionality git status

--- src/app/code-list/code-list.component.ts
+++ src/app/code-list/code-list.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Subscription } from "rxjs";
 import { BudgetCode } from "./../budget-code.model";
 import { CodeService } from "./../code.service";
+import { SEARCH_CATEGORIES, SORT_CATEGORIES } from '../constants';
 
 @Component({
   selector: "app-code-list",
@@ -10,18 +11,24 @@ import { CodeService } from "./../code.service";
   styleUrls: ["./code-list.component.css"],
 })
 export class CodeListComponent implements OnInit {
-  codes: BudgetCode[];
-  filteredCodes: BudgetCode[];
   searchForm: FormGroup;
-  searchCategories = ["Fiscal Year", "Budget Code", "Budget Title"];
+  sortForm: FormGroup;
+  codes: BudgetCode[] = [];
+  searchCategories = SEARCH_CATEGORIES;
+  sortCategories = SORT_CATEGORIES;
 
   constructor(private codeService: CodeService) {}
 
   ngOnInit() {
     this.searchForm = new FormGroup({
       searchKey: new FormControl(null),
-      searchCategory: new FormControl(this.searchCategories[0]),
+      searchCategory: new FormControl(null),
     });
+
+    this.sortForm = new FormGroup({
+      sortCategory: new FormControl(null)
+    });
+
     this.codeService.fetchCodes();
     this.codes = this.codeService.getCodes();
   }
@@ -37,7 +44,7 @@ export class CodeListComponent implements OnInit {
     );
 
     switch (catIndex) {
-      case 0:
+      case 0: // search by Fiscal Year
         const searchedYear = Number(key);
         if (searchedYear) {
           this.codes = this.codeService
@@ -45,12 +52,12 @@ export class CodeListComponent implements OnInit {
             .filter((code) => code.fiscalYear === searchedYear);
         }
         break;
-      case 1:
+      case 1: // Search by Budget Code
         this.codes = this.codeService
           .getCodes()
           .filter((code) => code.budgetCode.toLowerCase().indexOf(key.toLowerCase()) !== -1);
         break;
-      case 2:
+      case 2: // Search by Budget Title
         this.codes = this.codeService
           .getCodes()
           .filter((code) => code.budgetTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1);
@@ -59,7 +66,34 @@ export class CodeListComponent implements OnInit {
         this.codes = this.codeService.getCodes();
     }
   }
-  filterByYear() {
-    return this.codes.filter((code) => code.fiscalYear);
+
+  onSortCodes() {
+    const selSort = this.sortForm.get("sortCategory").value;
+    console.log(selSort);
+    const selSortIndex = this.sortCategories.indexOf(selSort);
+    switch (selSortIndex) {
+      case 0: //sort by Fiscal Year in ascending order
+        this.codes.sort((a, b) => (a.fiscalYear > b.fiscalYear) ? 1 : -1);
+        break;
+      case 1: //sort by Fiscal Year in descending order
+        this.codes.sort((a, b) => (a.fiscalYear < b.fiscalYear) ? 1 : -1);
+        break;
+      case 2: //sort by Budget Code in ascending order
+        this.codes.sort((a, b) => (a.budgetCode > b.budgetCode) ? 1 : -1);
+        break;
+      case 3: //sort by Budget Code in descending order
+        this.codes.sort((a, b) => (a.budgetCode < b.budgetCode) ? 1 : -1);
+        break;
+
+      case 4: //sort by Budget Title in ascending order
+        this.codes.sort((a, b) => (a.budgetTitle > b.budgetTitle) ? 1 : -1);
+        break;
+
+      case 2: //sort by Budget Title in descending order
+        this.codes.sort((a, b) => (a.budgetTitle > b.budgetTitle) ? 1 : -1);
+        break;
+      default:
+        break;
+    }
   }
 }
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,5 @@
 export const URL_ALL_CODES = "https://uat.trc.eku.edu/budgetcodeexam/api/all";
 export const URL_ADD_CODE = "https://uat.trc.eku.edu/budgetcodeexam/api/add";
+
+export const SEARCH_CATEGORIES = ["Fiscal Year", "Budget Code", "Budget Title"];
+export const SORT_CATEGORIES = ["Fiscal Year: ascending", "Fiscal Year: descending", "Budget Code: ascending", "Budget Code: descending", "Budget Title: ascending", "Budget Title: ascending"];
