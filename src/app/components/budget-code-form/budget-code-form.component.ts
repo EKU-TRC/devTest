5a0f3646d91ddaf588314a0ef2e7ab9829647899
Kenneth <kenneth.james.carroll@gmail.com>
2020-12-09 13:20:13 -0500

    Updated documentation on files modified for error handling

--- src/app/components/budget-code-form/budget-code-form.component.ts
+++ src/app/components/budget-code-form/budget-code-form.component.ts
@@ -2,8 +2,8 @@
  * Budget Code Form Component Typescript
  * 
  * author: Kenneth Carroll
- * date: 12/8/2020
- * revision: 2
+ * date: 12/9/2020
+ * revision: 3
  */
 
 // angular imports
@@ -24,18 +24,29 @@ export class BudgetCodeFormComponent implements OnInit, OnDestroy {
 
   // public budget code for handling the input from the form
   budgetCode: BudgetCode = new BudgetCode(null, null, null, null);
+
+  // create a subscription to verify form submission
   formStatusSub: Subscription;
 
   // add the budget code service upon construction
   constructor(private budgetCodeService: BudgetCodesService, private router: Router) { }
 
   ngOnInit() {
+    // initialize form sttaus subscription
     this.formStatusSub = this.budgetCodeService.getFormStatusListenter()
     .subscribe((result: {status: string, message: string}) => {
+
+      // if the submission is a success
       if(result.status === "Success") {
+
+        // alert user and return to original list
         alert("The budget code was successfully addeds!");
         this.router.navigate(['/']);
+
+        // submission failed
       } else {
+
+        // alert but remain on form to allow resubmission
         alert("The budget code was not added! Please verify that the code does not already exist.");
       }
     })
@@ -43,11 +54,12 @@ export class BudgetCodeFormComponent implements OnInit, OnDestroy {
 
   onSubmit() {
 
-    // submit the nudget code object from the form
+    // submit the budget code object from the form
     this.budgetCodeService.postNewBudgetCode(this.budgetCode);
 
   }
   ngOnDestroy() {
+    // remove subscription on destroy
     this.formStatusSub.unsubscribe();
   }
 
