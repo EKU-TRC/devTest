99abf893f6e2be223eca7d29973e683246136773
wwen0 <wwen0@users.noreply.github.com>
2020-12-10 12:39:23 -0500

    Update constants.ts
    corrected a typo

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,4 +2,4 @@ export const URL_ALL_CODES = "https://uat.trc.eku.edu/budgetcodeexam/api/all";
 export const URL_ADD_CODE = "https://uat.trc.eku.edu/budgetcodeexam/api/add";
 
 export const SEARCH_CATEGORIES = ["Fiscal Year", "Budget Code", "Budget Title"];
-export const SORT_CATEGORIES = ["Fiscal Year: ascending", "Fiscal Year: descending", "Budget Code: ascending", "Budget Code: descending", "Budget Title: ascending", "Budget Title: ascending"];
+export const SORT_CATEGORIES = ["Fiscal Year: ascending", "Fiscal Year: descending", "Budget Code: ascending", "Budget Code: descending", "Budget Title: ascending", "Budget Title: descending"];
